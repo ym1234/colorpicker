1f2cf93df6edfb1e9b6e14a0ed35158c872070c5
bl-ue <54780737+bl-ue@users.noreply.github.com>
2021-06-06 09:05:13 -0400

    Fix help
    the --oneshot option was printed as --one-shot

--- main.c
+++ main.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 			case 'h':
 				printf( "colorpicker [options]\n"
 						"  -h, --help:             show this help\n"
-						"  -o, --one-shot:         oneshot\n"
+						"  -o, --oneshot:          oneshot\n"
 						"  -q, --quit-on-keypress: quit on keypress\n"
 						"  -d, --hex:              hex only\n"
 						"  -r, --rgb:              rgb only\n");
